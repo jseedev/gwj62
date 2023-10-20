8e984d70354695946c87cfe28c17b9c769f16676
jseedev <jcloutier@123mail.org>
2023-10-19 20:08:43 -0400

    Added music switching mechanism, and "call ended" signal. Light anti-aliasing. Better environment lighting changes.

--- gd/Level.gd
+++ gd/Level.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal call_ended(call)
+
 var environments = {
 	day = {
 		sun_angle_max = 30.0,
@@ -21,22 +23,42 @@ func _ready():
 	lerp_environment(0.0)
 
 @onready var env:Environment = $WorldEnvironment.environment
+@onready var anim_light = $AnimatedLight
 func lerp_environment(night_weight):
+	var target_light
 	if night_weight >= 0.75:
-		$Moon.show()
-		$DuskSun.hide()
-		$MorningSun.hide()
+		target_light = $Moon
 	elif night_weight >= 0.5:
-		$Moon.hide()
-		$DuskSun.show()
-		$MorningSun.hide()
+		target_light=$DuskSun
 	else:
-		$Moon.hide()
-		$DuskSun.hide()
-		$MorningSun.show()
+		target_light = $MorningSun
+	anim_light.transform = anim_light.transform.interpolate_with(target_light.transform,night_weight)
+	anim_light.light_color = lerp(anim_light.light_color,target_light.light_color,night_weight)
+	anim_light.light_energy = lerp(anim_light.light_energy,target_light.light_energy,night_weight)
 	var mat = env.sky.sky_material
 	for prop in environments.day.keys():
 		if prop.find("fog") > -1:
 			env[prop]=lerp(environments.day[prop],environments.night[prop],night_weight)
 		else:
 			mat[prop]=lerp(environments.day[prop],environments.night[prop],night_weight)
+
+@onready var music_player = $Level/Music
+func _on_call_ended(call):
+	if call == "call2":
+		#change music to new tune
+		change_music(load("res://audio/music/music_2_acoustic_92bpm_loop.ogg"))
+
+		
+
+func change_music(new_track):
+	if music_player.playing:
+		var mtween = get_tree().create_tween()
+		mtween.tween_property(music_player,"volume_db",-50.0,2.0)
+		mtween.play()
+		await get_tree().create_timer(2.3).timeout
+		music_player.stop()
+	var vtween = get_tree().create_tween()
+	music_player.stream=new_track
+	music_player.play()
+	vtween.tween_property(music_player,"volume_db",-12.0,2.0)
+	vtween.play()
--- gd/phone.gd
+++ gd/phone.gd
@@ -2,16 +2,22 @@ extends MeshInstance3D
 var ringing = true
 var con_timer=0
 var on_call = null
-
+var call_playing = false
+@onready var call_player = $calls/AnimationPlayer
 func _physics_process(_delta):
 	if ringing and Input.is_action_just_released("answer_phone"):
 		answer()
 		play_call(on_call)
 	if visible and Input.is_action_just_pressed("ui_cancel"):
 		hang_up()
+	if call_playing and !call_player.is_playing():
+		call_playing=false
+		var current_level = get_tree().current_scene
+		current_level.emit_signal("call_ended",on_call)
+		
 
 func play_call(call_name):
-	$calls/AnimationPlayer.play(call_name)
+	call_player.play(call_name)
 
 func outgoing_call(to_name,call_name):
 	$CallFrom.text="Calling"
@@ -23,6 +29,7 @@ func outgoing_call(to_name,call_name):
 	await get_tree().create_timer(8.0).timeout
 	answer()
 	play_call(on_call)
+	
 	
 func incoming_call(from_name,call_name):
 	$PhoneIcon.show()
@@ -48,6 +55,7 @@ func answer():
 	$AnimationPlayer.play("Connected")
 	$ConnectedTime.show()
 	ringing=false
+	call_playing=true
 
 func connected():
 	con_timer+=1
